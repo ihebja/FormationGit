8152117daeb22c704796bfcfc1b3d1470c14fe50
ihebja <38739665+ihebja@users.noreply.github.com>
2023-06-19 16:50:32 +0200

    Update main.py delete main.o

--- fonctionnalite.py
+++ fonctionnalite.py
@@ -3,3 +3,4 @@
 print("fonctionnalité developpement terminé")
 print("Ce texte est écrit depuis master") 
 print("Ce texte est écrit depuis br1")
+print("test v6")
--- main.py
+++ main.py
@@ -3,3 +3,4 @@
 print("Bonjour tout le monde") 
 print("!!")
 print("Ce texte est écrit par un collègue lamda")
+print("ajout ligne par un collegue")
